42559822eaad8571b2f4d6b0b2038f6ce8fdb81f
SathishChekuri <chekurisathish41@gmail.com>
2026-08-02 17:58:37 +0530

    completed movie banner with navigation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,78 @@
 import React from 'react'
+
 import image from '../image.png'
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+
+import axios from "axios";
+
 function Banner() {
+
+  const [movies, setMovies] = useState([]);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+   useEffect(() => {
+
+  axios.get(
+
+  "https://api.themoviedb.org/3/movie/popular?api_key=fb7bb23f03b6994dafc674c074d01761&language=en-US&page=1"
+
+      )
+
+    .then((res) => {
+
+      setMovies(res.data.results.slice(0, 5));
+
+    });
+
+      }, []);
+
+      const nextMovie=()=>{
+
+        setCurrentIndex((prev)=>(prev+1)%movies.length);
+
+      }
+
+      const prevMovie=()=>{
+
+      setCurrentIndex((prev)=>(prev==0)?movies.length-1:prev-1);
+
+      }
+
   return (
-    
-    <div className='h-[20vh] md:h-[75vh] bg-cover bg-center  flex items-end' style={{backgroundImage:`url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIkHtsi8CVKnqNVw_3u6OAdp3KnVlzoi9Lvxyrhr27gA&s=10)`}}> 
-    <div className='text-white text-xl text-center w-full bg-blue-900/60'>Spider-Man Brand New Day</div></div>
+       <div
+
+        className="h-[30vh] md:h-[75vh] bg-cover bg-center flex items-end bg-gray-900 p-6 rounded-2xl"
+
+        style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${movies[currentIndex]?.backdrop_path})`,
+        backgroundSize: "70%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        }}
+        
+
+        >
+
+      <button onClick={prevMovie} className="absolute left-4 top-1/2 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black/70">
+
+        ❮
+
+        </button>
+
+        <button onClick={nextMovie} className="absolute right-4 top-1/2 -translate-y-1/2 bg-black/50 text-white p-3 rounded-full hover:bg-black/70">
+
+         ❯
+
+        </button> 
+
+        <div className='text-white text-xi w-full p-2 text-center bg-gray-900/60'>{movies[currentIndex]?.title}</div></div>
+
   )
+
 }
+
+
 
 export default Banner
